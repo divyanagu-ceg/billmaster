b7b6f0381a82d7998dde9c2bb73524f4be8c78f0
divya <you@example.com>
2019-04-16 17:26:40 -0400

    heroku deploy mysql

--- app_api/models/connection.js
+++ app_api/models/connection.js
@@ -1,10 +1,15 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
+  /*host: "localhost",
   user: "root",
   password: "",
-  database: "expense_tracker"
+  database: "expense_tracker"*/
+    host: "us-cdbr-iron-east-02.cleardb.net",
+    user: "b5ebd2674d719d",
+    password:"c359b5cf",
+    database:"heroku_f652ba62f3da7bb"
+    
 });
 console.log("In connection.js");
 
